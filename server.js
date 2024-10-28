0ca8511e9bf22a35ff7183ae4020dcf93e73e7ca
Victor Koome <koomev786@gmail.com>
2024-10-28 06:17:06 +0300

    establishing routes

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3500
-const path = require('path')
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3500;
+const path = require("path");
 
+app.get("^/$|index(.html)?", (req, res) => {
+  //the node way
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello world Backend Node')
-})
+//redirecting
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
 
-app.listen(PORT, ()=> {
-    console.log(`Example app running on Port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Example app running on Port ${PORT}`);
+});
