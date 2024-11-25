5109c7bed99f6ba1a7c92d37d0d114f77dd39c98
Victor Koome <koomev786@gmail.com>
2024-11-25 07:31:05 +0300

    refresh controller

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const employeesController = require("../../controllers/employeesController");
-const verifyJWT = require("../../middleware/verifyJWT")
 
 //multiple http methods per route
 router
   .route("/")
-  .get(verifyJWT, employeesController.getAllEmployees)
+  .get(employeesController.getAllEmployees)
   .post(employeesController.addEmployee)
   .put(employeesController.updateEmployee)
   .delete(employeesController.deleteEmployee);
